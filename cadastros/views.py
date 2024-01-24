98785fe8c2569cf4e0c8fb69af5ddea9bcbd970a
FranciscoGabrielCavalcanteCandido <fg.14.cavalcandido@gmail.com>
2024-01-24 11:38:46 -0300

    extra context, titulo das telas

--- cadastros/views.py
+++ cadastros/views.py
@@ -8,22 +8,26 @@ class TipoCreate(CreateView):
     model = Tipo
     fields = ["nome"]
     template_name = "cadastros/form-cadastro.html"
+    extra_context = {"titulo": "Cadastro de Tipo de Bebida"}
     success_url = reverse_lazy("inicio")
 
 class CidadeCreate(CreateView):
     model = Cidade
     fields = ["nome"]
     template_name = "cadastros/form-cadastro.html"
+    extra_context = {"titulo": "Cadastro de Cidade"}
     success_url = reverse_lazy("inicio")
 
 class UfCreate(CreateView):
     model = Uf
     fields = ["nome"]
     template_name = "cadastros/form-cadastro.html"
+    extra_context = {"titulo": "Cadastro de Estado"}
     success_url = reverse_lazy("inicio")
 
 class CachacaCreate(CreateView):
     model = Cachaca
     fields = ["nome", "tipo", "marca", "ano", "cidade", "uf", "teor", "observacoes"]
+    extra_context = {"titulo": "Cadastro de Bebida"}
     template_name = "cadastros/form-cadastro.html"
     success_url = reverse_lazy("inicio")
