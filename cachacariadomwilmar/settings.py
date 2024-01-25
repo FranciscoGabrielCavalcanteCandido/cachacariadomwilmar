dcf9b3103b8dec0035873603d7348fcfff57e3d2
Matheus Magalhães <matheusurbano2002@hotmail.com>
2024-01-25 11:47:00 -0300

    Carregando imagens e listando na tela

--- cachacariadomwilmar/settings.py
+++ cachacariadomwilmar/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 #Configuracoes da midia
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
